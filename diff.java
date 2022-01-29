95e71dd4865169626f356c447653c87030c2c520
Mayur Shinde <mjshinde0402@gmail.com>
2022-01-29 21:59:27 +0530

    added implementation of diff.java

--- diff.java
+++ diff.java
@@ -3,6 +3,6 @@ public class Diff{
         int a=55;
         int b=20;
         int diff=a-b;
-        System.out.println("The diff is ="+diff);
+        System.out.println("The diff of two no's is ="+diff);
     }
 }
